20e63c1388c0ff0ab679d81f4092214319cc19b0
guangbao123456 <2451947362@qq.com>
2018-12-07 08:13:10 +0800

    实训项目1

--- shixun/kugou/kugou/items.py
+++ shixun/kugou/kugou/items.py
@@ -12,4 +12,5 @@ class KugouItem(scrapy.Item):
     # define the fields for your item here like:
     author = scrapy.Field()
     sname = scrapy.Field()
+    singid = scrapy.Field()
 
--- shixun/kugou/kugou/spiders/ku.py
+++ shixun/kugou/kugou/spiders/ku.py
@@ -20,31 +20,38 @@ class KuSpider(scrapy.Spider):
         url='https://songsearch.kugou.com/song_search_v2?&keyword='+key+'&page=1&pagesize=30'
         yield scrapy.Request(url,callback=self.parse1,meta={'item':item})
     def parse1(self,response):
-        item=response.meta['item']
+
         names=re.findall('"SongName":"(.*?)"',response.text,re.S)
         print(names)
-        names=list(set(names))
+        #names=list(set(names))
         i=0
+        item = response.meta['item']
+        item['sname'] = names
         for n in names:
-            item['sname']=n
+
+            #item['sname']=n
 
             print('{}:{}'.format(i,n))
             i += 1
         singid=int(input('歌曲id：'))
+        item['singid']=singid
         key1=re.findall('"FileHash":"(.*?)"',response.text,re.S)[singid-1]
         href='https://wwwapi.kugou.com/yy/index.php?r=play/getdata&hash='+key1
+        print(href)
         #href='http://fs.w.kugou.com/201811300918/17345c58d3873fa224ef4e41c4be1cf6/G063/M03/06/11/'+key1+'.mp3'
         yield scrapy.Request(href,callback=self.parse2,meta={'item':item})
     def parse2(self,response):
 
-        #sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')
+
         item = response.meta['item']
+        singid=item['singid']
         playurl=re.findall('"play_url":"(.*?)"',response.text,re.S)[0].replace('\\','')
         # aname=re.findall('"author_name":"(.*?)"',response.text,re.S)[0].strip()
         # sname=re.findall('"song_name":"(.*?)"',response.text,re.S)[0]
         song=requests.get(playurl).content
 
-        with open(item['author']+'/'+item['sname']+'.mp3','wb')as f:
+
+        with open(item['author']+'/'+item['sname'][singid]+'.mp3','wb')as f:
             f.write(song)
         print("下载完成！")
         yield item
